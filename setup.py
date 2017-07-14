420b89eb7f7188804358d85cbf0b2bb69c8ec5b2
John Bradley <johnbradley2008@gmail.com>
2017-07-14 14:34:27 -0400

    second pass at setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='DukeBlinky',
         url='https://github.com/Duke-GCB/DukeBlinky',
         keywords='duke ldap grouper internet2',
         license='MIT',
-        packages=['blinky'],
+        packages=['blinky.py'],
         install_requires=[
           'python-ldap==2.4.41',
           'requests==2.18.1',
